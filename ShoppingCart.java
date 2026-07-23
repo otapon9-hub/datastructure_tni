5b8f9145b3b72195697c6c34f63737a9dc0c3be6
otapon9-hub <otapon9@gmail.com>
2026-07-23 17:07:10 +0700

    Lab04

---
+++ ShoppingCart.java
@@ -0,0 +1,9 @@
+
+public class ShoppingCart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TodoList.java
@@ -0,0 +1,26 @@
+
+public class TodoList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DoublyLinkedList todolist = new DoublyLinkedList();
+		
+		todolist.insert("Finisg Homework");
+		todolist.insert("Laundry");
+		todolist.insert("Group meeting");
+		
+		System.out.println(todolist.backwardtraversal());
+		
+		todoList.insert("Submit Report";)
+		System.out.println(todolist.traversal());
+		
+		todoList.insert("Buy Food";)
+		System.out.println(todolist.traversal());
+
+		todoList.insert("GO to Gym";)
+		System.out.println(todolist.traversal());
+
+	}
+
+}
